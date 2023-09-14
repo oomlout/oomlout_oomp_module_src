fde7506f69101dea73370ce7c10387aa1bbc25a2
Oom Lout <aaron@oomlout.com>
2023-09-14 16:35:23 +0100

    regen

--- action_generate_kicad_outputs.py
+++ action_generate_kicad_outputs.py
@@ -6,17 +6,15 @@ def go_through_directories():
     for root, dirs, files in os.walk("modules"):
         #go through all files
         for file in files:
-           
+            #check for a brd file
             
-            #
-            # checkb for a brd file
-            
-
- 
             filename = os.path.join(root, file)
+            filter = ["npoole"]
+            #make filter all lower case
+            filter = [x.lower() for x in filter]
             filter = [""]
             #if any of filter is in filename
-            if any(x in filename for x in filter):
+            if any(x in filename.lower() for x in filter):
                 if file.endswith(".kicad_pcb"):
                     oom_kicad.generate_outputs(filename=filename, overwrite=True)
 
